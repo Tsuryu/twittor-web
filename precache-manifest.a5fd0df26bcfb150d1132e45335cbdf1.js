15b1d433310f04e0f38d56996cc2a96fd960d500
unknown <vahnxp@gmail.com>
2020-07-02 19:10:41 -0300

    Updates

---
+++ precache-manifest.a5fd0df26bcfb150d1132e45335cbdf1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7353870e43e91c571536e86d6132fa6d",
+    "url": "/twittor-web/index.html"
+  },
+  {
+    "revision": "548ebf0f9c0cf9adc146",
+    "url": "/twittor-web/static/css/2.9f98d992.chunk.css"
+  },
+  {
+    "revision": "60c6e593eaa0318f1687",
+    "url": "/twittor-web/static/css/main.0b1d639b.chunk.css"
+  },
+  {
+    "revision": "548ebf0f9c0cf9adc146",
+    "url": "/twittor-web/static/js/2.cfcb04f9.chunk.js"
+  },
+  {
+    "revision": "711f510bee39d4d7153bd7cc1fecf8f9",
+    "url": "/twittor-web/static/js/2.cfcb04f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c6e593eaa0318f1687",
+    "url": "/twittor-web/static/js/main.0c17282a.chunk.js"
+  },
+  {
+    "revision": "c515052cdce6a84647e6",
+    "url": "/twittor-web/static/js/runtime-main.5f9b89f3.js"
+  },
+  {
+    "revision": "16e35662baa4ca9e4f4344fd51fb7ec6",
+    "url": "/twittor-web/static/media/camera.16e35662.svg"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/twittor-web/static/media/close.cad70081.svg"
+  },
+  {
+    "revision": "9b45fe492d3b935b1a283ece2e1b5ac8",
+    "url": "/twittor-web/static/media/date-birth.9b45fe49.svg"
+  },
+  {
+    "revision": "d3c57e3b34eea06328b31dbb81bcabc4",
+    "url": "/twittor-web/static/media/error-404.d3c57e3b.png"
+  },
+  {
+    "revision": "a108520eea84cd603eeaf8eeda7676b1",
+    "url": "/twittor-web/static/media/link.a108520e.svg"
+  },
+  {
+    "revision": "02f2070b4161235fe7d8909276457b53",
+    "url": "/twittor-web/static/media/location.02f2070b.svg"
+  },
+  {
+    "revision": "41f65f75cb0b41db6fe44ab4b074bacc",
+    "url": "/twittor-web/static/media/logo-white.41f65f75.png"
+  },
+  {
+    "revision": "1769587d7e1330e9d4241c30f672c7ff",
+    "url": "/twittor-web/static/media/logo.1769587d.png"
+  }
+]);
